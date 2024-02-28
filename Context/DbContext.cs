b7af6234ae53f5a7dc5fa8af540f028202e67e31
Ethan Lim <studentlimsoo@gmail.com>
2024-02-28 14:34:46 +0900

    초기 커밋.

---
+++ Context/DbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiwithEf.Model;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace WebApiwithEf.Context
+{
+    public class BankContext :DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            // 엔티티에 대한 테이블 이름 설정
+            modelBuilder.Entity<Person>()
+                .ToTable("Persons");
+
+            modelBuilder.Entity<Account>()
+                .ToTable("Accounts");
+
+            // 주키 설정 
+            modelBuilder.Entity<Person>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Account>()
+                .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Person>()
+                .HasOne(p => p.Account)
+                .WithOne( a => a.Person )
+                .HasForeignKey<Account>( a => a.UserId );
+
+        }
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=D662-ETHANLIM;Database=test05;Trusted_Connection=True;TrustServerCertificate=True");
+           
+        }
+    }
+}
---
+++ Model/Models.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiwithEf.Model
+{
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }   
+
+
+        // 한명의 사람은 하나의 계좌만 가질 수 있음
+        public Account Account {  get; set; }
+
+    }
+
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public int Balance { get; set; } = 0;
+
+        public int UserId { get; set; }
+
+        // 한 개의 계좌는 1명의 사람만 가지고 있을 수 있다. 
+        public Person Person { get; set; } = null;
+
+    }
+}
